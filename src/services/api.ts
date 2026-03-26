9c86dd3e97f4dabf92032c5e27add722df2e495e
Daniel <daniel_cardona82231@elpoli.edu.co>
2026-03-26 00:45:54 -0500

    Mongo funcionando, app funcionando

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 // src/services/api.ts
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_URL = 'https://pelipopapi.onrender.com';
 
 export interface LoginRequest {
   email: string;
